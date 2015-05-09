76ddd35ed0dcfa5c6c185ac965ce40fe73d0aa49
Adam Wead <amsterdamos@gmail.com>
2015-05-08 20:19:40 -0400

    Don't use Sufia's keyword field in batch edit

---
+++ spec/views/batch/edit.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'batch/edit.html.erb' do
+  let(:batch) { Batch.create }
+  let(:generic_file) do
+    GenericFile.new(title: ['some title']).tap do |f|
+      f.apply_depositor_metadata("bob")
+    end
+  end
+  let(:form) { Sufia::Forms::BatchEditForm.new(generic_file) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(stub_model(User))
+    assign :batch, batch
+    assign :form, form
+    render
+  end
+
+  it "should not show Sufia's keyword field" do
+    expect(render).not_to have_selector "input#generic_file_tag"
+  end
+
+end
