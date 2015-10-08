53c3b763a4e9836e2fc4d2ce80ff710c6fa21abe
Adam Wead <amsterdamos@gmail.com>
2015-10-07 21:38:21 -0400

    Shared form specs for Actor and Work

--- spec/forms/actor_edit_form_spec.rb
+++ spec/forms/actor_edit_form_spec.rb
@@ -1,17 +1,7 @@
 require 'rails_helper'
 
 describe ActorEditForm do
-  subject { described_class.new(Actor.new) }
 
-  describe ".model_attributes" do
-    let(:params) do 
-      ActionController::Parameters.new( document_ids: [""] )
-    end
-    subject { described_class.model_attributes(params) }
-
-    it "removes empty strings" do
-      expect(subject["document_ids"]).to be_empty
-    end
-  end
+  it_behaves_like "a form for a Citi resource"
 
 end
--- spec/forms/work_edit_form_spec.rb
+++ spec/forms/work_edit_form_spec.rb
@@ -1,17 +1,7 @@
 require 'rails_helper'
 
 describe WorkEditForm do
-  subject { described_class.new(Work.new) }
 
-  describe ".model_attributes" do
-    let(:params) do 
-      ActionController::Parameters.new( asset_ids: [""] )
-    end
-    subject { described_class.model_attributes(params) }
-
-    it "removes empty strings" do
-      expect(subject["asset_ids"]).to be_empty
-    end
-  end
+  it_behaves_like "a form for a Citi resource"
 
 end
