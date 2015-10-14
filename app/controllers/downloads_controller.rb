12a3ca29086e73dab32fe0f46d62096ca913c1d8
Adam Wead <amsterdamos@gmail.com>
2015-10-13 23:07:19 -0400

    Override Sufia::DownloadsControllerBehavior
    This should be bypassed when a trusted certificated is used with
    Fedora, see #113.

---
+++ app/controllers/downloads_controller.rb
@@ -0,0 +1,11 @@
+class DownloadsController < ApplicationController
+  include Sufia::DownloadsControllerBehavior
+
+  # Overrides Hydra::Controller::DownloadBehavior due to an issue with self-signed certificates, see issue #113
+  def send_file_contents
+    self.status = 200
+    prepare_file_headers
+    send_data(file.content)
+  end
+
+end
---
+++ spec/controllers/downloads_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe DownloadsController do
+
+  routes { Sufia::Engine.routes }
+
+  let(:user) { FactoryGirl.find_or_create(:jill) }
+  let(:asset) do
+    GenericFile.create do |gf|
+      gf.apply_depositor_metadata(user)
+      gf.assert_still_image
+    end
+  end
+
+  before do
+    allow(controller).to receive(:has_access?).and_return(true)
+    sign_in user
+    allow_any_instance_of(User).to receive(:groups).and_return([])
+    allow_any_instance_of(FileContentDatastream).to receive(:content).and_return("file content")
+    allow_any_instance_of(FileContentDatastream).to receive(:new_record?).and_return(false)
+    allow_any_instance_of(FileContentDatastream).to receive(:mime_type).and_return("text/plain")
+  end  
+
+  describe "#show" do
+
+    it "downloads a file's content" do
+      get :show, id: asset.id
+      expect(response).to be_success
+      expect(response.body).to eq("file content")
+    end
+
+  end
+
+end
