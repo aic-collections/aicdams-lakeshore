5e34e45af5e0d13017406d377252c50b22d27157
Adam Wead <amsterdamos@gmail.com>
2015-12-03 23:20:37 -0500

    Correct misconfigued ExhibtionsController test

--- spec/controllers/exhibition_controller_spec.rb
+++ spec/controllers/exhibition_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 describe ExhibitionsController do
-  it_behaves_like "a controller for a Citi resource", "actor"
+  it_behaves_like "a controller for a Citi resource", "exhibition"
 end
