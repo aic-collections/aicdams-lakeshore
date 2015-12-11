8a703215382f01fe2d4136a0c0dee613e64a53a9
Adam Wead <amsterdamos@gmail.com>
2015-12-10 23:44:25 -0500

    Adding production server configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
+server 'lakeshore.artic.edu', user: 'awead', roles: %w{web app db}
 
+set :linked_files, fetch(:linked_files, []).push('config/fedora-cert.pem')
 
 
 # role-based syntax
