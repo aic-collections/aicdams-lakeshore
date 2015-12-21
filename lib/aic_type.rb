3989725a996d62fd312534067c2d190b32219c86
Adam Wead <amsterdamos@gmail.com>
2015-12-21 10:47:35 -0500

    Adding additional vocabulary terms to AICType

--- lib/aic_type.rb
+++ lib/aic_type.rb
@@ -18,14 +18,46 @@ class AICType < RDF::StrictVocabulary("http://definitions.artic.edu/ontology/1.0
        subClassOf: "aictype:Annotation".freeze,
        label: "Comment".freeze,
        comment: "Comment".freeze
+  term :CommentCategory,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Comment category".freeze,
+       comment: "Comment category".freeze
+  term :CompositingType,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Compositing".freeze,
+       comment: "Compositing type".freeze
+  term :ConservationDocumentType,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Conservation document type".freeze,
+       comment: "Conservation document type".freeze
+  term :ConservationImageType,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Conservation image type".freeze,
+       comment: "Conservation image type".freeze
   term :ConservationMetadata,
        subClassOf: "aictype:MetadataSet".freeze,
        label: "Conservation Metadata".freeze,
        comment: "Conservation metadata".freeze
+  term :Department,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Department".freeze,
+       comment: "Department".freeze
+  term :DigitizationSource,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Digitization source".freeze,
+       comment: "Digitization source".freeze
+  term :DocumentType,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Document type".freeze,
+       comment: "Document type".freeze
   term :Exhibition,
        subClassOf: "aictype:CitiResource".freeze,
        label: "Exhibition".freeze,
        comment: "CITI Exhibition".freeze
+  term :LightType,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Light type".freeze,
+       comment: "Light type".freeze
   term :List,
        subClassOf: "aictype:Resource".freeze,
        label: "List".freeze,
@@ -53,6 +85,10 @@ class AICType < RDF::StrictVocabulary("http://definitions.artic.edu/ontology/1.0
        subClassOf: "aictype:Asset".freeze,
        label: "Still Image".freeze,
        comment: "Still image".freeze
+  term :Tag,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "Tag".freeze,
+       comment: "Tag".freeze
   term :Text,
        subClassOf: "aictype:Asset".freeze,
        label: "Text Document".freeze,
@@ -61,6 +97,10 @@ class AICType < RDF::StrictVocabulary("http://definitions.artic.edu/ontology/1.0
        subClassOf: "aictype:CitiResource".freeze,
        label: "Transaction".freeze,
        comment: "CITI Transaction".freeze
+  term :ViewType,
+       subClassOf: "aictype:ListItem".freeze,
+       label: "View Type".freeze,
+       comment: "View type".freeze
   term :Work,
        subClassOf: "aictype:CitiResource".freeze,
        label: "Work".freeze,
--- spec/lib/aic_type_spec.rb
+++ spec/lib/aic_type_spec.rb
@@ -11,16 +11,27 @@ describe AICType do
     expect(described_class.Asset).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.CitiResource).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Comment).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.CommentCategory).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.CompositingType).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.ConservationDocumentType).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.ConservationImageType).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.ConservationMetadata).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.Department).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.DigitizationSource).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.DocumentType).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Exhibition).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.LightType).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.List).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.ListItem).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.MetadataSet).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Resource).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Shipment).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.StatusType).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.StillImage).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.Tag).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Text).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Transaction).to be_kind_of RDF::Vocabulary::Term
+    expect(described_class.ViewType).to be_kind_of RDF::Vocabulary::Term
     expect(described_class.Work).to be_kind_of RDF::Vocabulary::Term
   end
 end
