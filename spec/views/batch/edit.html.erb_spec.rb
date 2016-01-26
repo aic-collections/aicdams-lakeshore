d44d7ffbd6537e9ae82af2c2c9061b0330a3fef3
Adam Wead <amsterdamos@gmail.com>
2016-01-25 23:15:19 -0500

    Hide fields, remove status from batch form, fixes #205, #210

--- spec/views/batch/edit.html.erb_spec.rb
+++ spec/views/batch/edit.html.erb_spec.rb
@@ -22,9 +22,10 @@ describe 'batch/edit.html.erb' do
   end
 
   it "draws the form" do
-    within("div#generic_file_document_type_ids") do
-      expect(page).to have_content('required="required')
-    end
-    expect(page).not_to have_selector "input#generic_file_resource_type"
+    within("div#generic_file_document_type_ids") { expect(page).to have_content('required="required') }
+    expect(page).not_to have_selector("input#generic_file_resource_type")
+    expect(page).not_to have_selector("select#generic_file_status_id")
+    within("div.generic_file_representation_for") { expect(page).to have_selector("input.hidden") }
+    within("div.generic_file_document_for") { expect(page).to have_selector("input.hidden") }
   end
 end
