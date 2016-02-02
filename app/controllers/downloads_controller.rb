824a2a17d16b1f38dc942898f06635bd47217958
Adam Wead <amsterdamos@gmail.com>
2016-02-02 16:01:14 -0500

    Pass file name to send_data method, fixes #209

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,9 +2,10 @@ class DownloadsController < ApplicationController
   include Sufia::DownloadsControllerBehavior
 
   # Overrides Hydra::Controller::DownloadBehavior due to an issue with self-signed certificates, see issue #113
+  # Uses Rails' ActionController::DataSteaming
   def send_file_contents
     self.status = 200
     prepare_file_headers
-    send_data(file.content)
+    send_data(file.content, filename: file_name)
   end
 end
