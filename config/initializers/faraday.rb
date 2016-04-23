9173787d1aa0900a29a58a80a4d096d0aa7650d2
Adam Wead <amsterdamos@gmail.com>
2016-04-22 22:20:36 -0400

    Don't verify SSL certs (for now)

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,3 +1,7 @@
 require 'faraday'
+require 'openssl'
+
+# Ouch. Only if we have self-signed certs.
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 cert = File.join(Rails.root, "config/fedora-cert.pem")
 Faraday.default_connection_options.ssl.ca_file = cert if File.exist?(cert)
