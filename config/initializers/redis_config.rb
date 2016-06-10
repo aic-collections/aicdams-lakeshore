b4bca82e6dd1afd4c08c2928fa5dc2d3b3194a9c
Adam Wead <amsterdamos@gmail.com>
2016-06-09 21:47:04 -0400

    Updating Redis initializer

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -8,36 +8,20 @@ if defined?(PhusionPassenger)
       config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
 
       # The important two lines
-      $redis.client.disconnect if $redis
-      $redis = begin
-                 Redis.new(host: config[:host], port: config[:port], thread_safe: true)
-               rescue
-                 nil
-               end
-      Resque.redis = $redis
-      Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{Rails.env}"
-      Resque.redis.client.reconnect if Resque.redis
+      Redis.current.disconnect!
+      Redis.current = begin
+                        Redis.new(config.merge(thread_safe: true))
+                      rescue
+                        nil
+                      end
     end
   end
 else
   config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-  $redis = begin
-             Redis.new(host: config[:host], port: config[:port], thread_safe: true)
+  require 'redis'
+  Redis.current = begin
+             Redis.new(config.merge(thread_safe: true))
            rescue
              nil
            end
-  Resque.redis = $redis
-  Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{Rails.env}"
-end
-
-# Code borrowed from Obie's Redis patterns talk at RailsConf'12
-Nest.class_eval do
-  def initialize(key, redis = $redis)
-    super(key.to_param)
-    @redis = redis
-  end
-
-  def [](key)
-    self.class.new("#{self}:#{key.to_param}", @redis)
-  end
 end
