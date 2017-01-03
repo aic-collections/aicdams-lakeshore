87e226d5936f5a51a492cdc8ffb8d1232dbb2776
Adam Wead <amsterdamos@gmail.com>
2017-01-02 21:40:14 -0500

    Set upload file size limit in uploader.js, ref #842

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,12 +32,3 @@
 //= require lakeshore/autocomplete
 //= require lakeshore/deleted_files
 //= require lakeshore/asset_workflow
-
-// Set upload limits
-// Overrides defaults in CurationConcerns uploader.js
-// 1 GB  max file size 
-max_file_size = 1073741824;
-max_file_size_str = "1 GB";
-// 100 GB max total upload size
-max_total_file_size = 107374182400;
-max_total_file_size_str = "100 GB";
