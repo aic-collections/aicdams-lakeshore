151a8b4ba6132f6c8ad5cfbe8c2d5c1c3532746b
Adam Wead <amsterdamos@gmail.com>
2017-01-18 22:49:44 -0500

    Allow a null prefix when UidMinter is initialized, ref #1380

--- app/services/uid_minter.rb
+++ app/services/uid_minter.rb
@@ -2,21 +2,20 @@
 class UidMinter
   attr_reader :prefix
 
-  # @param [String] prefix two-letter prefix indicating the type of resource
-  def initialize(prefix)
-    raise(ArgumentError, "Can't mint a UID without a prefix") unless prefix.present?
+  # @param [String, nil] prefix two-letter prefix indicating the type of resource
+  def initialize(prefix = nil)
     @prefix = prefix
   end
 
   # Returns a unique identifier with a two-letter prefix and six digits
   # @return [String] such as "TX-123456"
   def mint
+    raise(ArgumentError, "Can't mint a UID without a prefix") unless prefix.present?
     return mock_id unless Rails.env.production?
     uid_from_database
   end
 
-  # Returns the MD5 checksum of a UID, formatted as a pseudo-random UUID as per
-  # RFC 4122.
+  # Returns the MD5 checksum of a UID, formatted as a pseudo-random UUID as per RFC 4122.
   # @param [String] id
   # @return [String] Hash (with dashes added) of original UID
   def hash(id)
