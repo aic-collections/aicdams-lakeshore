2b4ad64cd5f931e2a2401d28ecad6a983b9ebcdd
Adam Wead <amsterdamos@gmail.com>
2017-03-11 15:15:39 -0500

    Updating server names

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 # AIC Lakeshore production server
 set :user, 'rbrun'
-server 'lakeshore.artic.edu', user: 'rbrun', roles: %w(web app db)
+server 'aicdams07.artic.edu', user: 'rbrun', roles: %w(web app db)
