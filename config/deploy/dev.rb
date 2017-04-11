5989505f48b727cd5efdd6e09291cd35b9a3c873
Adam Wead <amsterdamos@gmail.com>
2017-04-10 21:24:38 -0400

    Update dev delopyment to match other environments
    Removes unneeded step that deletes pre-compiled assets because we're no longer deploying as a Rails development environment.

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
-# Deploy using Rails development environment to enable local development on the server directly
-set :rails_env, 'development'
+# AIC Lakeshore development server
 set :user, 'rbrun'
 server 'aicdamsdev01.artic.edu', user: 'rbrun', roles: %w(web app db)
-
-# An inelegant hack: delete the precompiled assets because we're in development mode and don't need them
-namespace :deploy do
-  after :finished, :delete_assets do
-    on roles(:web) do
-      execute "rm -Rf #{fetch(:deploy_to)}/current/public/assets/*"
-    end
-  end
-end
