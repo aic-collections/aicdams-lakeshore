15bfbbeec9647b230c444d4ce6fdb6d21510d2fb
Adam Wead <amsterdamos@gmail.com>
2017-05-02 22:34:04 -0400

    Add imaging UID to CITI notification message, ref #1725
    Adds additional information to the response sent to CITI for resources with preferred representations.
    
    Refactors the response to use a CitiNotification class that encompasses all the data requirements of the response.

---
+++ app/models/citi_notification.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+# Data object representing a notification sent to CITI when the representative image
+# of an asset is changed.
+class CitiNotification
+  attr_reader :file_set, :citi_resource
+
+  delegate :citi_uid, to: :citi_resource
+
+  # @param [FileSet] file_set
+  # @param [CitiResource] citi_resource such as a work, exhibit
+  def initialize(file_set, citi_resource)
+    @file_set = file_set
+    @citi_resource = citi_resource
+  end
+
+  # @return [String]
+  # This is main method used in the class. Returns a json object that will be
+  # send to CITI with the details of a given resource and its related file set.
+  def to_json
+    {
+      uid: uid,
+      password: password,
+      citi_uid: citi_uid,
+      type: type,
+      image_uid: image_uid,
+      last_modified: last_modified,
+      asset_details: asset_details
+    }.to_json
+  end
+
+  protected
+
+    def uid
+      ENV.fetch("citi_api_uid")
+    end
+
+    def password
+      ENV.fetch("citi_api_password")
+    end
+
+    def type
+      citi_resource.class.to_s
+    end
+
+    def last_modified
+      return unless file_set && file_set.original_file
+      file_set.original_file.fcrepo_modified.first.iso8601
+    end
+
+    def image_uid
+      return unless file_set
+      file_set.id
+    end
+
+    def asset_details
+      return {} unless citi_resource.respond_to?(:imaging_uid)
+      { image_number: citi_resource.imaging_uid }
+    end
+end
---
+++ spec/models/citi_notification_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe CitiNotification do
+  subject { described_class.new(file_set, citi_resource) }
+
+  let(:citi_resource) { build(:work, id: "fakeid", citi_uid: "WO-fake") }
+  let(:file_set)      { nil }
+
+  context "without a file set" do
+    its(:uid)           { is_expected.to eq(ENV.fetch("citi_api_uid")) }
+    its(:password)      { is_expected.to eq(ENV.fetch("citi_api_password")) }
+    its(:type)          { is_expected.to eq("Work") }
+    its(:citi_uid)      { is_expected.to eq("WO-fake") }
+    its(:image_uid)     { is_expected.to be_nil }
+    its(:last_modified) { is_expected.to be_nil }
+    its(:asset_details) { is_expected.to be_empty }
+    its(:to_json)       { is_expected.to match(/Work/) }
+  end
+
+  context "with a file set and citi resource" do
+    let(:file_set)  { build(:file_set, id: "fakefsid") }
+    let(:mock_file) { double("MockFile", fcrepo_modified: [DateTime.parse("July 14")]) }
+
+    before { allow(file_set).to receive(:original_file).and_return(mock_file) }
+
+    its(:uid)           { is_expected.to eq(ENV.fetch("citi_api_uid")) }
+    its(:password)      { is_expected.to eq(ENV.fetch("citi_api_password")) }
+    its(:type)          { is_expected.to eq("Work") }
+    its(:citi_uid)      { is_expected.to eq("WO-fake") }
+    its(:image_uid)     { is_expected.to eq("fakefsid") }
+    its(:last_modified) { is_expected.to eq("2017-07-14T00:00:00+00:00") }
+    its(:to_json)       { is_expected.to match(/fakefsid/) }
+  end
+
+  context "with an asset with an image_uid" do
+    let(:citi_resource) { build(:asset, imaging_uid: ["fakeimageuid"]) }
+
+    its(:asset_details) { is_expected.to eq(image_number: ["fakeimageuid"]) }
+  end
+end
