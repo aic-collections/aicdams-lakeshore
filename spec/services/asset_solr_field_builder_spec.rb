b2d4d062aa1ab129d88099d05194e773674efd1e
Adam Wead <amsterdamos@gmail.com>
2017-05-11 12:35:37 -0500

    Extracting asset indexer tests to AssetSolrFieldBuilder
    By extracting the behaviors to a new class, tests can be written in an isolated way using non-persisted objects.
    
    This greatly reduces the amount of time needed to run the tests.

---
+++ spec/services/asset_solr_field_builder_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe AssetSolrFieldBuilder do
+  let(:asset)                     { build(:still_image_asset) }
+  let(:representations)           { Array.wrap(build(:work)) }
+  let(:preferred_representations) { Array.wrap(build(:work)) }
+
+  let(:mock_relationships) do
+    instance_double("Inbound::Relationships", attachments: ["attachment"],
+                                              documents: ["document"],
+                                              representations: representations,
+                                              preferred_representations: preferred_representations,
+                                              preferred_representation: preferred_representations.first)
+  end
+
+  subject { described_class.new(asset) }
+
+  context "with no relationships" do
+    its(:attachment_facet)               { is_expected.to be_nil }
+    its(:attachment_of_facet)            { is_expected.to be_nil }
+    its(:documentation_facet)            { is_expected.to be_nil }
+    its(:representation_facet)           { is_expected.to be_nil }
+    its(:preferred_representation_facet) { is_expected.to be_nil }
+    its(:related_works)                  { is_expected.to include_json([]) }
+    its(:main_ref_numbers)               { is_expected.to be_empty }
+  end
+
+  context "with relationships" do
+    before { allow(subject).to receive(:relationships).and_return(mock_relationships) }
+
+    its(:attachment_of_facet)            { is_expected.to eq("Has Attachment") }
+    its(:documentation_facet)            { is_expected.to eq("Documentation For") }
+    its(:representation_facet)           { is_expected.to eq("Is Representation") }
+    its(:preferred_representation_facet) { is_expected.to eq("Is Preferred Representation") }
+  end
+
+  context "when the subject is an attachment of an asset" do
+    before { allow(asset).to receive(:attachments).and_return(["attachment"]) }
+    its(:attachment_facet) { is_expected.to eq("Is Attachment") }
+  end
+
+  describe "related_works" do
+    before { allow(subject).to receive(:relationships).and_return(mock_relationships) }
+
+    context "with missing work data" do
+      let(:representations) { Array.wrap(build(:work, :with_sample_metadata, citi_uid: "", main_ref_number: "")) }
+
+      its(:related_works) { is_expected.to include_json([]) }
+      its(:main_ref_numbers) { is_expected.to be_empty }
+    end
+
+    context "with one relationship" do
+      let(:representations) { Array.wrap(build(:work, :with_sample_metadata)) }
+
+      its(:related_works) { is_expected.to include_json([{ citi_uid: "43523", main_ref_number: "1999.397" }]) }
+      its(:main_ref_numbers) { is_expected.to contain_exactly("1999.397") }
+    end
+
+    context "with two relationships" do
+      let(:representations) do
+        [
+          build(:work, :with_sample_metadata),
+          build(:work, :with_sample_metadata, citi_uid: "12345", main_ref_number: "2017.392")
+        ]
+      end
+
+      its(:related_works) { is_expected.to include_json([{ citi_uid: "43523", main_ref_number: "1999.397" },
+                                                         { citi_uid: "12345", main_ref_number: "2017.392" }]) }
+      its(:main_ref_numbers) { is_expected.to contain_exactly("1999.397", "2017.392") }
+    end
+
+    context "with a preferred representation" do
+      let(:preferred_representations) { Array.wrap(build(:work, citi_uid: "12345", main_ref_number: "2017.392")) }
+
+      its(:related_works) { is_expected.to include_json([{ citi_uid: "12345", main_ref_number: "2017.392" }]) }
+      its(:main_ref_numbers) { is_expected.to contain_exactly("2017.392") }
+    end
+
+    context "with several preferred representations" do
+      let(:preferred_representations) do
+        [
+          build(:work, citi_uid: "54321", main_ref_number: "1720.392"),
+          build(:work, citi_uid: "12345", main_ref_number: "2017.392")
+        ]
+      end
+
+      its(:related_works) { is_expected.to include_json([{ citi_uid: "54321", main_ref_number: "1720.392" },
+                                                         { citi_uid: "12345", main_ref_number: "2017.392" }]) }
+      its(:main_ref_numbers) { is_expected.to contain_exactly("1720.392", "2017.392") }
+    end
+  end
+end
