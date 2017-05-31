2e765e4cdcdc9e078323cbabf63a66cb2e1b0012
Adam Wead <amsterdamos@gmail.com>
2017-05-30 22:24:57 -0400

    "On behalf of" parameter in download API, ref #1788
    The download API should accept a parameter for a user whose rights will be evaluated for downloading a given resource.
    
    The admin rights of the API user are ignored if a proxied, "on behalf of" user is supplied.

--- app/controllers/lakeshore/downloads_controller.rb
+++ app/controllers/lakeshore/downloads_controller.rb
@@ -9,8 +9,20 @@ module Lakeshore
       # Overrides CurationConcerns::DownloadBehavior to return 404 instead of 302
       def authorize_download!
         return head :unauthorized unless ActiveFedora::Base.exists?(params[asset_param_key])
-        return params[:id] if current_user.admin? || current_ability.can?(:read, params[asset_param_key])
+        return params[:id] if evaluated_as_admin? || proxy_ability.can?(:read, params[asset_param_key])
         head :unauthorized
+      end
+
+    private
+
+      # Only check admin access for the api user if no proxy user is specified
+      def evaluated_as_admin?
+        return false if params.key?(:on_behalf_of)
+        current_user.admin?
+      end
+
+      def proxy_ability
+        Ability.new(User.find_by_email(params.fetch(:on_behalf_of, nil)))
       end
   end
 end
--- spec/controllers/lakeshore/downloads_controller_spec.rb
+++ spec/controllers/lakeshore/downloads_controller_spec.rb
@@ -10,22 +10,39 @@ describe Lakeshore::DownloadsController do
   before { sign_in_basic(apiuser) }
 
   describe "#show" do
+    before { allow(controller).to receive(:citi_thumbnail).and_return(file.path) }
+
     context "with a CITI thumbnail" do
-      before do
-        allow(controller).to receive(:citi_thumbnail).and_return(file.path)
-        get :show, id: my_file.id, file: "citiThumbnail"
-      end
+      before { get :show, id: my_file.id, file: "citiThumbnail" }
       its(:response) { is_expected.to be_successful }
     end
 
     context "with an unknown file" do
       before { get :show, id: my_file.id, file: "badFile" }
-      its(:status) { is_expected.to eq(404) }
+      its(:response) { is_expected.to be_not_found }
     end
 
     context "with an unknown FileSet" do
       before { get :show, id: "xxxy", file: "citiThumbnail" }
-      its(:status) { is_expected.to eq(401) }
+      its(:response) { is_expected.to be_unauthorized }
+    end
+
+    context "on behalf of another user who has access" do
+      before { get :show, id: my_file.id, file: "citiThumbnail", on_behalf_of: "user1" }
+      its(:response) { is_expected.to be_successful }
+    end
+
+    context "on behalf of another user who does not have access" do
+      before do
+        create(:user2)
+        get :show, id: my_file.id, file: "citiThumbnail", on_behalf_of: "user2"
+      end
+      its(:response) { is_expected.to be_unauthorized }
+    end
+
+    context "on behalf of a bogus user" do
+      before { get :show, id: my_file.id, file: "citiThumbnail", on_behalf_of: "asdf" }
+      its(:response) { is_expected.to be_unauthorized }
     end
   end
 end
