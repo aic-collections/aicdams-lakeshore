f99caca1a8b5390cc015421fbe1502882cdba693
Adam Wead <amsterdamos@gmail.com>
2017-08-08 22:35:57 -0400

    Refactoring CurationConcerns#show
    Using one show view for all curation concerns types and present
    different layouts based on the presenter differences.

--- app/controllers/curation_concerns/generic_works_controller.rb
+++ app/controllers/curation_concerns/generic_works_controller.rb
@@ -11,34 +11,7 @@ class CurationConcerns::GenericWorksController < ApplicationController
 
   def manifest
     headers['Access-Control-Allow-Origin'] = '*'
-    render json:
-      ManifestService.new(presenter).manifest_builder
-  end
-
-  # overriding CC show action to render the html template for assets, which renders the representative media element in the page's full column width, not sidebar
-  def show
-    respond_to do |wants|
-      wants.html do
-        presenter && parent_presenter
-        render "curation_concerns/base/show_asset.html.erb", status: :ok
-      end
-      wants.json do
-        # load and authorize @curation_concern manually because it's skipped for html
-        @curation_concern = _curation_concern_type.find(params[:id]) unless curation_concern
-        authorize! :show, @curation_concern
-        render :show, status: :ok
-      end
-      additional_response_formats(wants)
-      wants.ttl do
-        render body: presenter.export_as_ttl, content_type: 'text/turtle'
-      end
-      wants.jsonld do
-        render body: presenter.export_as_jsonld, content_type: 'application/ld+json'
-      end
-      wants.nt do
-        render body: presenter.export_as_nt, content_type: 'application/n-triples'
-      end
-    end
+    render json: ManifestService.new(presenter).manifest_builder
   end
 
   def destroy
@@ -50,7 +23,6 @@ class CurationConcerns::GenericWorksController < ApplicationController
     end
     report_status_and_redirect(asset_with_relationships, batch: false)
   end
-  # we have to override this action in Curation Concerns application controller behavior class
 
   def deny_access
     presenter = UnauthorizedPresenter.new(params[:id])
