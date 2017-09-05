5a4c42cd3e534ff0b19f9f9abb8c9d2d80df92ef
Adam Wead <amsterdamos@gmail.com>
2017-09-04 21:53:24 -0400

    Updating FileSet unit tests
    Moves a spec test from the model to the indexer where it can be properly
    mocked.

--- spec/indexers/file_set_indexer_spec.rb
+++ spec/indexers/file_set_indexer_spec.rb
@@ -6,6 +6,12 @@ describe FileSetIndexer do
     let(:file_set) { build(:intermediate_file_set) }
     let(:solr_doc) { described_class.new(file_set).generate_solr_document }
 
+    before do
+      allow(file_set).to receive(:file_size).and_return(["1234"])
+      allow(file_set).to receive(:height).and_return(["12"])
+      allow(file_set).to receive(:width).and_return(["8"])
+    end
+
     it "indexes RDF types" do
       expect(solr_doc["rdf_types_ssim"]).to include(AICType.IntermediateFileSet)
       expect(solr_doc["aic_depositor_ssim"]).to eq(file_set.aic_depositor.nick)
@@ -13,6 +19,12 @@ describe FileSetIndexer do
       expect(solr_doc["dept_created_tesim"]).to eq(file_set.dept_created.pref_label)
       expect(solr_doc["dept_created_sim"]).to eq(file_set.dept_created.pref_label)
       expect(solr_doc["dept_created_citi_uid_ssim"]).to eq(file_set.dept_created.citi_uid)
+    end
+
+    it "indexes technical information about the file" do
+      expect(solr_doc[Solrizer.solr_name("file_size", :stored_sortable, type: :integer)]).to eq "1234"
+      expect(solr_doc[Solrizer.solr_name("image_width", :searchable, type: :integer)]).to eq 8
+      expect(solr_doc[Solrizer.solr_name("image_height", :searchable, type: :integer)]).to eq 12
     end
   end
 end
--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -6,14 +6,6 @@ describe FileSet do
 
   subject { file }
 
-  describe "#to_solr" do
-    xit "contains our custom solr fields" do
-      expect(subject.to_solr[Solrizer.solr_name("file_size", :stored_sortable, type: :integer)]).to eq "1234"
-      expect(subject.to_solr[Solrizer.solr_name("image_width", :searchable, type: :integer)]).to eq 8
-      expect(subject.to_solr[Solrizer.solr_name("image_height", :searchable, type: :integer)]).to eq 12
-    end
-  end
-
   describe "#id" do
     let(:file) { create(:department_file) }
     its(:id) { is_expected.to match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/) }
