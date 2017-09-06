f6573f41935449e45004ce243fd7bbfe209cada6
Adam Wead <amsterdamos@gmail.com>
2017-09-05 21:48:50 -0400

    Allow for empty RDF types when updating, ref #2067
    Running the list manager task was causing an error in sparql update
    syntax because there were no RDF types to delete.
    
    RDFTypeChangeService will not perform any sparql update if there are
    neither types to add or delete.

--- spec/services/rdf_type_change_service_spec.rb
+++ spec/services/rdf_type_change_service_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 describe RDFTypeChangeService do
   let(:resource)  { create(:original_file_set) }
   let(:new_types) { [AICType.IntermediateFileSet, AICType.CompositingType] }
+  let(:service)   { described_class.new(resource, new_types) }
 
   it "changes the rdf types of a resource" do
     expect(resource.type.map(&:to_s)).to contain_exactly("http://pcdm.org/models#Object",
@@ -23,5 +24,35 @@ describe RDFTypeChangeService do
                                                          "http://definitions.artic.edu/ontology/1.0/type/CompositingType",
                                                          "http://fedora.info/definitions/v4/repository#Resource",
                                                          "http://www.w3.org/ns/ldp#RDFSource")
+  end
+
+  context "when there are no types to delete" do
+    before { allow(service).to receive(:types_to_delete).and_return([]) }
+
+    it "does not perform an update" do
+      expect(service).not_to receive(:sparql_update)
+      expect(service.delete).to eq(service)
+    end
+  end
+
+  context "when there are no types to add" do
+    before { allow(service).to receive(:types_to_add).and_return([]) }
+
+    it "does not perform an update" do
+      expect(service).not_to receive(:sparql_update)
+      expect(service.insert).to eq(service)
+    end
+  end
+
+  context "when there are no types to add or delete" do
+    before do
+      allow(service).to receive(:types_to_delete).and_return([])
+      allow(service).to receive(:types_to_add).and_return([])
+    end
+
+    it "does not perform any updates" do
+      expect(service).not_to receive(:sparql_update)
+      expect(service.delete.insert).to eq(service)
+    end
   end
 end
