8016d231f9bb7011b441e45a4e25e4717418648e
Adam Wead <amsterdamos@gmail.com>
2018-03-20 21:42:54 -0400

    Set asset as preferred representation in ingest API
    Redmine issue #1794
    
    This builds off of the existing behavior and adds additional features
    such as the force option. The previously scripted api examples are now
    complete tests.

--- app/models/lakeshore/ingest.rb
+++ app/models/lakeshore/ingest.rb
@@ -5,7 +5,7 @@ module Lakeshore
 
     attr_reader :ingestor, :submitted_asset_type, :document_type_uri, :original_file,
                 :intermediate_file, :presevation_master_file, :legacy_file, :additional_files, :params,
-                :preferred_representation_for
+                :preferred_representation_for, :force_preferred_representation
 
     validates :ingestor, :asset_type, :document_type_uri, :intermediate_file, presence: true
 
@@ -57,6 +57,11 @@ module Lakeshore
       end
     end
 
+    def force_preferred_representation?
+      return true if force_preferred_representation == "true" || force_preferred_representation == 1
+      false
+    end
+
     private
 
       def register_terms(metadata)
@@ -64,6 +69,7 @@ module Lakeshore
         @document_type_uri = metadata.fetch(:document_type_uri, nil)
         @preferred_representation_for = metadata.fetch(:preferred_representation_for, [])
         @ingestor = find_or_create_user(metadata.fetch(:depositor, nil))
+        @force_preferred_representation = metadata.fetch(:force_preferred_representation, false)
       end
 
       def find_or_create_user(key)
@@ -119,6 +125,7 @@ module Lakeshore
       end
 
       def additional_uploads
+        return [] unless additional_files
         additional_files.values.map do |file|
           Sufia::UploadedFile.create(file: file, user: ingestor)
         end
