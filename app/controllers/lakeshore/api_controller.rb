ec71cc480e4da1ef7c477fa4628e837d5ce48a01
Kevin Musiorski <kmusio@artic.edu>
2018-06-20 21:11:26 -0500

    validate depositor before authentication only on filesets controller

--- app/controllers/lakeshore/api_controller.rb
+++ app/controllers/lakeshore/api_controller.rb
@@ -1,25 +1,32 @@
 # frozen_string_literal: true
 module Lakeshore
   class APIController < ActionController::Base
+    before_action :validate_depositor, if: :new_controller_that_has_depositor_at_base_level
     before_action :authenticate_api_user
+
+    # check that depositor exists as AICUser
+    def validate_depositor
+      unless AICUser.find_by_nick(params[:depositor])
+        render plain: "AICUser '#{params[:depositor]}' not found, please review the depositor.\n", status: 400
+      end
+    end
+
+    # return true for controllers that will have depositor at base level
+    def new_controller_that_has_depositor_at_base_level
+      controller_name == "file_sets"
+    end
 
     def authenticate_api_user
       authenticate_or_request_with_http_basic do |username, password|
         resource = User.find_by_email(username)
         return head :unauthorized unless resource
         if resource.valid_password?(password) && resource.api?
-          if params[:depositor]
+          if params[:depositor] # need an if since ingest api does not have this param. new as of 1.7.5
             sign_in :user, User.find_by_email!(params[:depositor])
           else
             sign_in :user, resource
           end
         end
-      end
-    end
-
-    def validate_depositor
-      unless AICUser.find_by_nick(params[:depositor])
-        render plain: "AICUser '#{params[:depositor]}' not found, contact collections_support@artic.edu\n", status: 500
       end
     end
   end
