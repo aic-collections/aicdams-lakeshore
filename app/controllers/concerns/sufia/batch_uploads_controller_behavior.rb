1c44be153e4ec67f94dbd93e460ba2b034b745da
Kevin Musiorski <1021154+RudyOnRails@users.noreply.github.com>
2018-07-03 17:24:29 -0500

    1434: (GUI) User is taken to "Files shared with me" tab instead of "My Assets" after creating asset (#495)
    https://cits.artic.edu/issues/1434
    
    * Bug #1434: User is taken to "Files shared with me" tab instead of "My Assets" after creating asset
    
    * bundle exec rubocop --auto-correct --only FrozenStringLiteralComment

---
+++ app/controllers/concerns/sufia/batch_uploads_controller_behavior.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+module Sufia
+  module BatchUploadsControllerBehavior
+    extend ActiveSupport::Concern
+    include Hydra::Controller::ControllerBehavior
+    include CurationConcerns::CurationConcernController
+
+    included do
+      self.curation_concern_type = form_class.model_class
+    end
+
+    def create
+      authenticate_user!
+      create_update_job
+      flash[:notice] = t('sufia.works.new.after_create_html', application_name: view_context.application_name)
+      redirect_after_update
+    end
+
+    module ClassMethods
+      def form_class
+        ::Sufia::Forms::BatchUploadForm
+      end
+    end
+
+    protected
+
+      # Gives the class of the form.
+      # This overrides CurationConcerns
+      def form_class
+        self.class.form_class
+      end
+
+      def redirect_after_update
+        if uploading_on_behalf_of?
+          redirect_to sufia.dashboard_shares_path
+        else
+          redirect_to sufia.dashboard_works_path
+        end
+      end
+
+      def create_update_job
+        log = BatchCreateOperation.create!(user: current_user,
+                                           operation_type: "Batch Create")
+        BatchCreateJob.perform_later(current_user,
+                                     params[:title],
+                                     params[:resource_type],
+                                     params[:uploaded_files],
+                                     attributes_for_actor,
+                                     log)
+      end
+
+      def uploading_on_behalf_of?
+        params.fetch(hash_key_for_curation_concern).fetch(:on_behalf_of, nil).present?
+      end
+  end
+end
