a1c2a833ef77ce6a34e4f99f4d088906341d39fd
Adam Wead <amsterdamos@gmail.com>
2019-08-05 23:33:42 -0400

    report object to delete/update assets from csv

--- lib/report.rb
+++ lib/report.rb
@@ -17,16 +17,53 @@ class Report
                                  .map { |row| row['preferred_representation'] }.first
       asset_to_keep = set.max_by { |row| row['master_height'].to_i }
       assets_to_delete = set.reject { |row| row['uid'] == asset_to_keep['uid'] }
-      update(asset: asset_to_keep, preferred_representation: preferred_representation)
-      delete(assets_to_delete)
+
+      RemoveDuplicateJob.perform_later(
+        asset_to_keep: asset_to_keep['uid'],
+        assets_to_delete: assets_to_delete.map { |asset| asset['uid'] },
+        preferred_representation: preferred_representation
+      )
     end
   end
 
-  def update(asset:, preferred_representation: nil)
-    puts "Update #{asset['uid']}"
-  end
+  class RemoveDuplicateJob < ActiveJob::Base
+    queue_as :release
 
-  def delete(assets)
-    puts "Delete #{assets.map { |row| row['uid'] }.join(', ')}"
+    def perform(asset_to_keep:, assets_to_delete:, preferred_representation: nil)
+      update(asset_uid: asset_to_keep, preferred_representation: preferred_representation)
+      assets_to_delete.each { |asset| delete(asset) }
+    end
+
+    def update(asset_uid:, preferred_representation:)
+      asset = GenericWork.find_by_uid(asset_uid)
+      return unless asset
+
+      if preferred_representation
+        citi_resource = ActiveFedora::Base.find(preferred_representation)
+        service = InboundRelationshipManagementService.new(asset)
+        service.add_or_remove(:representations, [preferred_representation])
+        citi_resource.preferred_representation_uri = asset.uri.to_s
+        citi_resource.save
+      end
+
+      asset.reload
+      asset.publish_channels = [web_channel]
+      asset.save
+    end
+
+    def delete(asset_uid)
+      asset = GenericWork.find_by_uid(asset_uid)
+      return unless asset
+
+      service = InboundRelationshipManagementService.new(asset)
+      service.add_or_remove(:representations, [])
+      service.add_or_remove(:documents, [])
+      service.add_or_remove(:attachments, [])
+      asset.delete
+    end
+
+    def web_channel
+      PublishChannel.find("http://definitions.artic.edu/publish_channel/Web")
+    end
   end
 end
